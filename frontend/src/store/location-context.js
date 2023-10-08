5ad15072d9a621e6f377bfe654b8c4ce75d0b1bd
Niyoj Oli <oliniyoj@gmail.com>
2023-10-08 14:16:09 +0545

    compeltes weather api fetching

--- frontend/src/store/location-context.js
+++ frontend/src/store/location-context.js
@@ -6,7 +6,7 @@ const LocationContext = React.createContext({
 	country: "nepal",
 	countryCode: "NPL",
 	city: "bhaktapur",
-
+	weather: {},
 	onChangeLocation: (lat, lng) => {},
 });
 
@@ -16,7 +16,22 @@ export const LocationContextProvider = (props) => {
 		longitude: 85.429,
 		country: "nepal",
 		countryCode: "NPL",
+        weather: {},
 		city: "bhaktapur",
+	});
+
+	const [weatherData, setWeatherData] = useState({
+		weatherDescription: "",
+		icon: "",
+		temperature: 0,
+		minTemp: 0,
+		maxTemp: 0,
+		prssure: 0,
+		humidity: 0,
+		visibility: 0,
+		windSpeed: 0,
+		sunrise: 0,
+		sunset: 0,
 	});
 
 	const locationChangeHandler = (lat, lng) => {
@@ -25,13 +40,32 @@ export const LocationContextProvider = (props) => {
 		)
 			.then((response) => response.json())
 			.then((data) => {
-				console.log(data);
 				setDataPos({
 					latitude: lat,
 					longitude: lng,
 					country: data.features[0].properties.country,
 					countryCode: data.features[0].properties.country_code,
 					city: data.features[0].properties.county,
+				});
+			});
+
+		fetch(
+			`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lng}&units=metric&appid=e73ab71246cd8b4a162482332030a10d`
+		)
+			.then((response) => response.json())
+			.then((data) => {
+				setWeatherData({
+					weatherDescription: data.weather[0].description,
+					icon: data.weather[0].icon,
+					temperature: data.main.temp,
+					minTemp: data.main.temp_min,
+					maxTemp: data.main.temp_max,
+					pressure: data.main.pressure,
+					humidity: data.main.humidity,
+					visibility: data.visibility,
+					windSpeed: data.wind.speed,
+					sunrise: data.sys.sunrise,
+					sunset: data.sys.sunset,
 				});
 			});
 	};
@@ -45,6 +79,7 @@ export const LocationContextProvider = (props) => {
 				countryCode: dataPos.countryCode,
 				city: dataPos.city,
 
+				weather: { ...weatherData },
 				onChangeLocation: locationChangeHandler,
 			}}
 		>
